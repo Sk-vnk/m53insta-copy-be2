5bd33bb3b8554d0794a90ccafbd9a3fa36886729
Xavier Pashley <xavipashley@gmail.com>
2024-01-16 15:54:33 +0000

    chore: user add function works

--- src/server.js
+++ src/server.js
@@ -3,12 +3,29 @@ const express = require("express");
 
 const port = process.env.PORT || 5002;
 
+const User = require("./user/model");
+
+const userRouter = require("./user/routes");
+
 const app = express();
+
+app.use(express.json());
+app.use(userRouter);
+
+const syncTables = async () => {
+    await User.sync();
+};
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "API is health"});
+});
 
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "API is health" });
 });
 
+
 app.listen(port, () => {
+    syncTables();
     console.log("Server is listening");
 });
